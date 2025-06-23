8282419ff49470ec1151e873febeef22064e02bd
Surajsharma0804 <surajkumarind08@gmail.com>
2025-06-23 19:06:10 +0530

    Normalize line endings

---
+++ Day 1/Printing Even Number.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter n: ";
+    cin>> n;
+    if(n%2==0) {
+        cout<< n <<": is Even Number";
+    } else {
+        cout<< n << ": is Odd Number";
+    }
+    return 0;
+}
