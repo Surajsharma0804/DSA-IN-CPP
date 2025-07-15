a9ccd58c501c0480caf156b181d3a45a757bbe87
Surajsharma0804 <surajkumarind08@gmail.com>
2025-07-15 18:37:47 +0530

    Revision

---
+++ Day 2/FindingReaminder.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n, m;
+    cout<< "Enter n: ";
+    cin>> n;
+    cout << "Enter m: ";
+    cin >> m;
+    cout<< "Remainder : " <<n%m;
+    return 0;
+}
---
+++ Day 2/subtraction.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int x, y;
+    cout << "Enter x: ";
+    cin>> x;
+    cout << "Enter y: ";
+    cin >> y;
+    cout << x + y << endl;
+    return 0;
+}
