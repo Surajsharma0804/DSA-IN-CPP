77d872502fc7ae491597cd454eaeab845d6fef3b
Surajsharma0804 <surajkumarind08@gmail.com>
2025-07-15 12:54:42 +0530

    Revision of basics

---
+++ Day 2/normalsum.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main () {
+    int n, m;
+    cout << "Enter n: ";
+    cin>> n;
+    cout << "Enter m: ";
+    cin >> m;
+    cout <<"Sum of Two number: " << n + m;
+    return 0;
+}
